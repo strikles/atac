d4ff15aa163d1a4f082e7f12285700f5894a99d1
strikles <strikles@gmail.com>
2021-05-30 10:59:59 +0000

    fix

--- atac/scrape.py
+++ atac/scrape.py
@@ -112,12 +112,14 @@ class UnderTheMangoTree:
                                     
             unique_contacts = None
             if type == "email":
-                unique_contacts = filter(lambda e: e not in self.emails, new_contacts)
+                unique_contacts = list(filter(lambda e: e not in self.emails, new_contacts))
             elif type == "phone":
-                unique_contacts = filter(lambda e: e not in self.phones, new_contacts)
+                unique_contacts = list(filter(lambda e: e not in self.phones, new_contacts))
                 
             print("\x1b[6;37;41m new {0}:{1} | {2} \x1b[0m".format(type, len(unique_contacts), unique_contacts))
-            map(writer.writerow, unique_contacts)
+            for contact in unique_contacts:
+                writer.writerow(contact)
+                
 
     def process_page(self, data_key, starting_url):
 
