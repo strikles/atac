20c0603654eb74464fcd9ac793062daa39a604b3
strikles <strikles@gmail.com>
2021-07-11 07:04:43 +0000

    email_validation

--- atac/clean.py
+++ atac/clean.py
@@ -29,7 +29,7 @@ class Leon:
                                   
         return is_valid
                                   
-    def clean_files(self, path):
+    def cleanup(self, path):
         
         print(path)
         status = 0
