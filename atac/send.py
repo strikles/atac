3023e891660f4a9aa5d065fe318d8d8774c4a46a
strikles <strikles@gmail.com>
2021-09-03 06:51:37 +0000

    revert

--- atac/send.py
+++ atac/send.py
@@ -8,9 +8,15 @@ import markdown
 import time
 from bs4 import BeautifulSoup
 from validator_collection import checkers
+
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+
+from twilio.rest import Client
+import facebook
+import tweepy
 from random import randint
+
 from tqdm import tqdm
 
 class FromRuXiaWithLove:
@@ -19,29 +25,6 @@ class FromRuXiaWithLove:
         self.config = {}
         with open('auth.json') as json_file:
             self.config = json.load(json_file)
-            
-    def compose_email(self, content, auth, mailing_list):
-       # compose email
-        message = MIMEMultipart("alternative")
-        message["Subject"] = content['subject']
-        message["From"] = auth['sender']
-        message["To"] = mailing_list
-        # Create the plain-text and HTML version of your message
-        text = ""
-        html = ""
-        # convert markdown to html
-        md = 'assets/mail_content/' + content['markdown']
-        with open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', md)), 'r') as f:
-            ptext = f.read()
-            html = markdown.markdown(ptext)
-        # Turn these into plain/html MIMEText objects
-        part1 = MIMEText(text, "plain")
-        part2 = MIMEText(html, "html")
-        # Add HTML/plain-text parts to MIMEMultipart message
-        # The email client will try to render the last part first
-        message.attach(part1)
-        message.attach(part2)
-        return message
         
     def send_email(self, path):
         
@@ -49,8 +32,7 @@ class FromRuXiaWithLove:
         status = 0
         # get mailing list csv files
         ml_files = list(filter(lambda c: c.endswith('.csv'), os.listdir(path)))
-        batch_size = min(1, 2000)
-        
+
         # reload config
         with open('auth.json') as json_file:
             self.config = json.load(json_file)
@@ -61,7 +43,7 @@ class FromRuXiaWithLove:
         # get active content
         content_ndx = email_cfg['active_content']
         content = email_cfg['content'][content_ndx]
-        
+
         # set sctive to next and save config
         if email_cfg['rotate_content']:
             email_cfg['active_content'] = (1 + content_ndx) % len(email_cfg['content'])
@@ -71,30 +53,29 @@ class FromRuXiaWithLove:
         with open('auth.json', 'w') as fp:
             self.config['send']['email'] = email_cfg
             json.dump(self.config, fp, indent=4)
-        
+
         for ml in ml_files:
             cf = path + ml
             print(cf)
-            
             with open(cf) as file:
                 lines = [line for line in file]
-                num_batches = len(lines) // batch_size
+                num_batches = len(lines)
                 ml_emails = [[] for i in range(num_batches)]
                 ml_counter = 0
 
                 with tqdm(total=len(lines)) as progress:
                     for ndx, receiver_email in csv.reader(lines):
                         if checkers.is_email(receiver_email):           
-                            ml_emails[ml_counter // num_batches].append(receiver_email)
+                            ml_emails[ml_counter].append(receiver_email)
                             ml_counter += 1
                         progress.update(1)
-
+                        
                 with tqdm(total=len(ml_emails)) as progress2:
                     for ml_batch in ml_emails:
                         mailing_list = '; '.join(ml_batch)
                         print(mailing_list)
-
-                        # compose email
+                        
+                        # Send email here
                         message = MIMEMultipart("alternative")
                         message["Subject"] = content['subject']
                         message["From"] = auth['sender']
@@ -114,8 +95,8 @@ class FromRuXiaWithLove:
                         # The email client will try to render the last part first
                         message.attach(part1)
                         message.attach(part2)
-                        
                         # Create secure connection with server and send email
+                        
                         try:
                             context = ssl.create_default_context()
                             with smtplib.SMTP_SSL(auth['server'], auth['port'], context=context) as server:
@@ -125,10 +106,10 @@ class FromRuXiaWithLove:
                             print(f'\x1b[6;37;41m error occurred: {err}\x1b[0m')
                         finally:
                             print("\x1b[6;37;42m Sent \x1b[0m")
-
+                            
                         time.sleep(5)
                         progress2.update(1)
-                
+                    
         return status
 
     def send_whatsapp(self, path):
@@ -169,3 +150,36 @@ class FromRuXiaWithLove:
                         )
                         print(message.sid)
         return status
+
+    def send_facebook(self):
+        status = 0
+        msg = "Hello, world!"
+        graph = facebook.GraphAPI(self.config['facebook']['access_token'])
+        link = 'https://www.jcchouinard.com/'
+        groups = ['744128789503859']
+        for group in groups:
+            graph.put_object(group, 'feed', message=msg, link=link)
+            print(graph.get_connections(group, 'feed'))
+        return status
+
+    def send_twitter(self):
+        status = 0
+        CONSUMER_KEY = self.config['twitter']['consumer_key']
+        CONSUMER_SECRET = self.config['twitter']['consumer_secret']
+        ACCESS_TOKEN = self.config['twitter']['access_token']
+        ACCESS_TOKEN_SECRET = self.config['twitter']['access_token_secret']
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
+        handles = sys.argv[1]
+        f = open(handles, "r")
+        h = f.readlines()
+        f.close()
+        for i in h:
+            i = i.rstrip()
+            m = i + " " + sys.argv[2]
+            s = api.update_status(m)
+            nap = randint(1, 60)
+            time.sleep(nap)
+        return status
+        
