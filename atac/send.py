def5c33c7fb79ddc4c3a45a4b4270c61beda6456
strikles <strikles@gmail.com>
2021-09-03 06:05:05 +0000

    send

--- atac/send.py
+++ atac/send.py
@@ -92,7 +92,27 @@ class FromRuXiaWithLove:
                 with tqdm(total=len(ml_emails)) as progress2:
                     for ml_batch in ml_emails:
                         mailing_list = '; '.join(ml_batch)
-                        message = self.compose_email(content, auth, mailing_list)
+
+                        # compose email
+                        message = MIMEMultipart("alternative")
+                        message["Subject"] = content['subject']
+                        message["From"] = auth['sender']
+                        message["To"] = mailing_list
+                        # Create the plain-text and HTML version of your message
+                        text = ""
+                        html = ""
+                        # convert markdown to html
+                        md = 'assets/mail_content/' + content['markdown']
+                        with open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', md)), 'r') as f:
+                            ptext = f.read()
+                            html = markdown.markdown(ptext)
+                        # Turn these into plain/html MIMEText objects
+                        part1 = MIMEText(text, "plain")
+                        part2 = MIMEText(html, "html")
+                        # Add HTML/plain-text parts to MIMEMultipart message
+                        # The email client will try to render the last part first
+                        message.attach(part1)
+                        message.attach(part2)
                         
                         # Create secure connection with server and send email
                         try:
