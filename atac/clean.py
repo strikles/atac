7d91815a19ea7c0f85150a71904dbc94f15491cf
strikles <strikles@gmail.com>
2021-11-02 23:38:53 +0000

    whatsapp

--- atac/clean.py
+++ atac/clean.py
@@ -50,6 +50,7 @@ class Leon:
 
     def __init__(self):
         self.config = Config()
+        self.config.load_config()
 
 
     def clean_phones(self, path):
--- atac/scrape.py
+++ atac/scrape.py
@@ -30,6 +30,7 @@ class UnderTheMangoTree:
         self.num_phones = 0
         self.num_emails = 0
         self.config = Config()
+        self.config.load_config()
         self.scrape = self.config.data['scrape']
 
 
