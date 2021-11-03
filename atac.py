6470e41c4da5170d81cfdbe1cfeea9649a3cde2c
strikles <strikles@gmail.com>
2021-11-03 12:03:13 +0000

    config

--- atac.py
+++ atac.py
@@ -3,6 +3,7 @@ import os
 import sys
 import json
 from threading import Thread
+from PIL import Image
 import atac
 
 
@@ -144,8 +145,36 @@ def clean(arguments):
         leon.clean_phones(path_phones)
 
 
+def colete_voador(image_path):
+    # pass the image as command line argument
+    img = Image.open(image_path)
+    # resize the image
+    width, height = img.size
+    aspect_ratio = height/width
+    new_width = 120
+    new_height = aspect_ratio * new_width * 0.55
+    img = img.resize((new_width, int(new_height)))
+    # new size of image
+    # print(img.size)
+    # convert image to greyscale format
+    img = img.convert('L')
+    pixels = img.getdata()
+    # replace each pixel with a character from array
+    chars = ["B","S","#","&","@","$","%","*","!",":","."]
+    new_pixels = [chars[pixel//25] for pixel in pixels]
+    new_pixels = ''.join(new_pixels)
+    # split string of chars into multiple strings of length equal to new width and create a list
+    new_pixels_count = len(new_pixels)
+    ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
+    ascii_image = "\n".join(ascii_image)
+
 # create the top-level parser
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    prog='PROG',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=textwrap.dedent()
+)
+
 subparsers = parser.add_subparsers()
 
 # create the parser for the "email command
