603aef920d282bdc4209cb601fb07b808390176c
strikles <strikles@gmail.com>
2021-11-03 15:17:37 +0000

    config

--- atac/config.py
+++ atac/config.py
@@ -42,7 +42,18 @@ class SingletonMeta(type):
                 return cls._instances[cls]
 
 
-class Config(metaclass=SingletonMeta):
+
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+        return getinstance
+
+
+@singleton
+class Config:
 
     def __init__(self):
         self.key = None
