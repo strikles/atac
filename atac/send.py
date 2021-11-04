cc2052ca3742026f6d887d3f4e8ed09e99113722
strikles <strikles@gmail.com>
2021-11-04 10:19:06 +0000

    num_emails

--- atac/send.py
+++ atac/send.py
@@ -190,7 +190,7 @@ class FromRuXiaWithLove:
             print(cf)
             with open(cf) as file:
                 lines = [line for line in file]
-                num_emails_per_bucket = 100
+                num_emails_per_bucket = 77
                 num_buckets = len(lines) // num_emails_per_bucket
                 ml_emails = [[] for i in range(num_buckets)]
                 self.store_emails_in_buckets(lines, ml_emails)
