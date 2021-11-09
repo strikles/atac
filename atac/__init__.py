0979b39efda6dd594870a8ebe411a2e53f4067bf
strikles <strikles@gmail.com>
2021-11-09 23:33:27 +0000

    tests

--- atac/__init__.py
+++ atac/__init__.py
@@ -3,3 +3,5 @@ from .compose import AllTimeHigh
 from .clean import Leon
 from .scrape import UnderTheMangoTree
 from .send import FromRuXiaWithLove
+
+__all__ = ['Config', 'AllTimeHigh', 'Leon', 'UnderTheMangoTree', 'FromRuXiaWithLove',]
