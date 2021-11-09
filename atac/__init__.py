0fe45ac244ab4c4b97ba3397b4e64658b61b94fc
strikles <strikles@gmail.com>
2021-11-09 23:21:26 +0000

    tests

--- atac/__init__.py
+++ atac/__init__.py
@@ -2,4 +2,4 @@ from .config import Config
 from .compose import AllTimeHigh
 from .clean import Leon
 from .scrape import UnderTheMangoTree
-from .send import FromRuxiaWithLve
+from .send import FromRuxiaWithLove
