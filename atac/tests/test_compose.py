38afb959bc1aae255caddb5112e86d978b6523b9
strikles <strikles@gmail.com>
2021-11-15 13:06:20 +0000

    content

--- atac/tests/test_compose.py
+++ atac/tests/test_compose.py
@@ -11,5 +11,5 @@ key_file = None
 def test_markov():
     path = os.path.abspath('..')
     content = path + '/atac/assets/pg1009.txt'
-    two_back = atac.AllTimeHigh(None, None, None)
+    two_back = atac.AllTimeHigh(encrypted_config, config_file, key_file)
     assert(len(two_back.generate_markov_content(content)) < 200) is True
