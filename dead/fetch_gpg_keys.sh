4444b98fff527336a8749db5f94584e8e3687036
strikles <strikles@gmail.com>
2021-11-16 18:05:15 +0000

    send

---
+++ dead/fetch_gpg_keys.sh
@@ -0,0 +1,29 @@
+CSV of email address
+csv=$1
+
+# Get headers from CSV
+headers=$(head -1 $csv)
+
+# Find the column number of the email address
+emailCol=$(echo $headers | tr ',' '\n' | grep -n "email" | cut -d':' -f1)
+
+# Content of the CSV at emailCol column, skipping the first line
+emailAddrs=$(tail -n +2 $csv | cut -d',' -f$emailCol)
+gpgListPatrn='(?<entropy>\d+)\s*bit\s*(?<algo>\S+)\s*key\s*(?<pubkeyid>[^,]+)'
+# Loop through the array and get the public keys
+for email in "${emailAddrs[@]}"
+do
+    # Get the public key ids for the email address by matching the regex gpgListPatt
+    pubkeyids=$(gpg --batch --keyserver hkp://keyserver.ubuntu.com --search-keys $email 2>&1 | grep -Po $gpgListPatrn | cut -d' ' -f5)
+    # For each public key id, get the public key
+    for pubkeyid in $pubkeyids
+    do
+        # Add the public key to the local keyring
+        recvr=$(gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys $pubkeyids 2>&1)
+        # Check exit code to see if the key was added
+        if [ $? -eq 0 ]; then
+            # If the public key is added, do some extra work with it
+            # [do stuff]
+        fi
+    done
+done
