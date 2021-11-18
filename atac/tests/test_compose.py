98a1bdeaa7dc41ee52fc02031ea1d15c8b9a1851
strikles <strikles@gmail.com>
2021-11-18 12:49:32 +0000

    cleanup

--- atac/tests/test_compose.py
+++ atac/tests/test_compose.py
@@ -7,29 +7,24 @@ encrypted_config = True
 config_file = 'auth.json'
 key_file = None
 
-corpus = "Nel mezzo del cammin di nostra vita"
-  "mi ritrovai per una selva oscura,"
-  "ché la diritta via era smarrita."
-
-  "Ahi quanto a dir qual era è cosa dura"
-  "esta selva selvaggia e aspra e forte"
-  "che nel pensier rinova la paura!"
-
-  "Tant è amara che poco è più morte;"
-  "ma per trattar del ben chi vi trovai,"
-  'dirò de laltre cose chi vho scorte"
-
-  "Io non so ben ridir com i vintrai,"
-  "tant era pien di sonno a quel punto"
-  "che la verace via abbandonai."
-
-  "Ma poi chi fui al piè dun colle giunto,"
-  "là dove terminava quella valle"
-  "che mavea di paura il cor compunto,"
-
-  "guardai in alto e vidi le sue spalle"
-  "vestite già de raggi del pianeta"
-  "che mena dritto altrui per ogne calle."
+corpus = "Nel mezzo del cammin di nostra vita "
+"mi ritrovai per una selva oscura, "
+"ché la diritta via era smarrita. "
+"Ahi quanto a dir qual era è cosa dura "
+"esta selva selvaggia e aspra e forte "
+"che nel pensier rinova la paura! "
+"Tant è amara che poco è più morte; "
+"ma per trattar del ben chi vi trovai, "
+'dirò de laltre cose chi vho scorte "
+"Io non so ben ridir com i vintrai, "
+"tant era pien di sonno a quel punto "
+"che la verace via abbandonai. "
+"Ma poi chi fui al piè dun colle giunto, "
+"là dove terminava quella valle "
+"che mavea di paura il cor compunto, "
+"guardai in alto e vidi le sue spalle "
+"vestite già de raggi del pianeta "
+"che mena dritto altrui per ogne calle. "
 
 def test_markov():
     path = os.path.abspath('..')
