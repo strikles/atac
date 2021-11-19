45f85eda86e4e39b535ca8755de0ed29b9ab4857
strikles <strikles@gmail.com>
2021-11-19 02:50:46 +0000

    cleanup

--- atac.py
+++ atac.py
@@ -40,6 +40,7 @@ def config(arguments):
     if getattr(arguments, "decrypted_config_file"):
         decrypted_config_file_path = getattr(arguments, "decrypted_config_file")
         if decrypted_config_file_path:
+            config.load_config()
             config.save_config(decrypted_config_file_path, False)
 
 
