13e4f71318c01840f04ff0aac1770e9c25c59e04
strikles <strikles@gmail.com>
2021-11-21 11:29:44 +0000

    pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-    name = 'atac',         # How you named your package folder (MyLib)
-    packages = ['atac'],   # Chose the same as "name"
-    version = '0.1',      # Start with a small number and increase it with every change you make
+    name='atac',         # How you named your package folder (MyLib)
+    packages=['atac'],   # Chose the same as "name"
+    version='0.1',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description = 'activism tools',   # Give a short description about your library
-    author = 'strikles',                   # Type in your name
-    author_email = 'strikles@gmail.com',      # Type in your E-Mail
-    url = 'https://github.com/strikles/atac',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/strikles/atac/archive/v_01.tar.gz',    # I explain this later on
-    keywords = ['activism', 'scrape', 'cybertorture'],   # Keywords that define your package best
+    description='activism tools',   # Give a short description about your library
+    author='strikles',                   # Type in your name
+    author_email='strikles@gmail.com',      # Type in your E-Mail
+    url='https://github.com/strikles/atac',   # Provide either the link to your github or to your website
+    download_url='https://github.com/strikles/atac/archive/v_01.tar.gz',    # I explain this later on
+    keywords=['activism', 'scrape', 'cybertorture'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
         'ascii-magic',
         'beautifulsoup4',
