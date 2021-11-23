26dfbb533e96839832103ff333b32b524385c1ef
strikles <strikles@gmail.com>
2021-11-23 12:44:46 +0000

    pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 setup(
-    name='atac',         # How you named your package folder (MyLib)
-    packages=['atac'],   # Chose the same as "name"
+    name='atac',        # How you named your package folder (MyLib)
+    packages=['atac'],  # Chose the same as "name"
     version='0.1',      # Start with a small number and increase it with every change you make
-    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license='MIT',      # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='activism tools',   # Give a short description about your library
     author='strikles',                   # Type in your name
     author_email='strikles@gmail.com',      # Type in your E-Mail
@@ -47,11 +47,15 @@ setup(
         'yowsup2'
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',              # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',              # Define that your audience are developers
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',       # Again, pick a license
-        'Programming Language :: Python :: 3',          # Specify which pyhton versions that you want to support
+        # Set the license
+        'License :: OSI Approved :: MIT License',
+        # Specify the pyhton versions supported
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
