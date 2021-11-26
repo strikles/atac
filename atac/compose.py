a35eb0f46d62ad96eae3be77a1849621569c22a3
strikles <strikles@gmail.com>
2021-11-26 17:08:53 +0000

    invaders

--- atac/compose.py
+++ atac/compose.py
@@ -304,7 +304,7 @@ class Fractal:
         n3 = np.empty((width, height))
         for i in range(width):
             for j in range(height):
-                n3[i,j] = mandelbrot(r1[i] + 1j*r2[j], max_iter)
+                n3[i,j] = self.mandelbrot(r1[i] + 1j*r2[j], max_iter)
         return (r1, r2, n3)
 
     @staticmethod
@@ -312,26 +312,26 @@ class Fractal:
         dpi = 72
         img_width = dpi * width
         img_height = dpi * height
-        x, y, z = mandelbrot_set(x_min, x_max, y_min, y_max, img_width, img_height, max_iter)
-
+        x, y, z = self.mandelbrot_set(x_min, x_max, y_min, y_max, img_width, img_height, max_iter)
+        #
         fig, ax = plt.subplots(figsize=(width, height), dpi=72)
         ticks = np.arange(0, img_width, 3*dpi)
         x_ticks = x_min + (x_max - x_min)*ticks/img_width
         plt.xticks(ticks, x_ticks)
         y_ticks = y_min + (y_max - y_min)*ticks/img_width
         plt.yticks(ticks, y_ticks)
-
+        #
         plt.axis('off')
         norm = colors.PowerNorm(0.3)
         ax.imshow(z.T, cmap=cmap, origin='lower')
         plt.show()
-        save_image(fig)
+        self.save_image(fig)
 
     @staticmethod
     def save_image(fig):
         filename = "mandelbrot.png"
         fig.savefig(filename)
-        mandelbrot_image(-2.0, 0.5, -1.25, 1.25, cmap='viridis')
+        self.mandelbrot_image(-2.0, 0.5, -1.25, 1.25, cmap='viridis')
 
 class Invader:
     """
@@ -341,7 +341,7 @@ class Invader:
     r = lambda: random.randint(50,255)
     rc = lambda: ('#%02X%02X%02X' % (r(),r(),r()))
     listSym = []
-
+    #
     @staticmethod
     def create_square(border, draw, randColor, element, size):
         if (element == int(size/2)):
