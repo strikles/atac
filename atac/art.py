91de9f59491d17bbd52d1012d648b5a70e257930
strikles <strikles@gmail.com>
2021-11-26 23:16:18 +0000

    gif

--- atac/art.py
+++ atac/art.py
@@ -160,6 +160,12 @@ def create_image(text, window_height, window_width):
     img.save('content.jpg')
 
 
+def make_gif(frame_folder):
+    frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.JPG")]
+    frame_one = frames[0]
+    frame_one.save("my_awesome.gif", format="GIF", append_images=frames, save_all=True, duration=100, loop=0)
+
+
 def create_qr_code(url):
     """
     Generate QR Code
