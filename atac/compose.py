8dcda856ac83c32a13473840e9744341d6d766ac
strikles <strikles@gmail.com>
2021-11-26 17:02:27 +0000

    invaders

--- atac/compose.py
+++ atac/compose.py
@@ -285,45 +285,111 @@ class AllTimeHigh(Config):
         # save it to a file
         img.save("qr.png")
 
+class Fractal:
+    """
+    """
+    def mandelbrot(z, max_iter):
+        c = z
+        for n in range(max_iter):
+            if abs(z) > 2:
+                return n
+            z = z*z + c
+        return max_iter
 
-def mandelbrot(z, max_iter):
-    c = z
-    for n in range(max_iter):
-        if abs(z) > 2:
-            return n
-        z = z*z + c
-    return max_iter
+    def mandelbrot_set(x_min, x_max, y_min, y_max, width, height, max_iter):
+        r1 = np.linspace(x_min, x_max, width)
+        r2 = np.linspace(y_min, y_max, height)
+        n3 = np.empty((width, height))
+        for i in range(width):
+            for j in range(height):
+                n3[i,j] = mandelbrot(r1[i] + 1j*r2[j], max_iter)
+        return (r1, r2, n3)
 
-def mandelbrot_set(x_min, x_max, y_min, y_max, width, height, max_iter):
-    r1 = np.linspace(x_min, x_max, width)
-    r2 = np.linspace(y_min, y_max, height)
-    n3 = np.empty((width, height))
-    for i in range(width):
-        for j in range(height):
-            n3[i,j] = mandelbrot(r1[i] + 1j*r2[j], max_iter)
-    return (r1, r2, n3)
+    def mandelbrot_image(x_min, x_max, y_min, y_max, width=10, height=10, max_iter=80, cmap='hot'):
+        dpi = 72
+        img_width = dpi * width
+        img_height = dpi * height
+        x, y, z = mandelbrot_set(x_min, x_max, y_min, y_max, img_width, img_height, max_iter)
 
-def mandelbrot_image(x_min, x_max, y_min, y_max, width=10, height=10, max_iter=80, cmap='hot'):
-    dpi = 72
-    img_width = dpi * width
-    img_height = dpi * height
-    x, y, z = mandelbrot_set(x_min, x_max, y_min, y_max, img_width, img_height, max_iter)
+        fig, ax = plt.subplots(figsize=(width, height), dpi=72)
+        ticks = np.arange(0, img_width, 3*dpi)
+        x_ticks = x_min + (x_max - x_min)*ticks/img_width
+        plt.xticks(ticks, x_ticks)
+        y_ticks = y_min + (y_max - y_min)*ticks/img_width
+        plt.yticks(ticks, y_ticks)
 
-    fig, ax = plt.subplots(figsize=(width, height), dpi=72)
-    ticks = np.arange(0, img_width, 3*dpi)
-    x_ticks = x_min + (x_max - x_min)*ticks/img_width
-    plt.xticks(ticks, x_ticks)
-    y_ticks = y_min + (y_max - y_min)*ticks/img_width
-    plt.yticks(ticks, y_ticks)
+        plt.axis('off')
+        norm = colors.PowerNorm(0.3)
+        ax.imshow(z.T, cmap=cmap, origin='lower')
+        plt.show()
+        save_image(fig)
 
-    plt.axis('off')
-    norm = colors.PowerNorm(0.3)
-    ax.imshow(z.T, cmap=cmap, origin='lower')
-    plt.show()
-    save_image(fig)
+    def save_image(fig):
+        filename = "mandelbrot.png"
+        fig.savefig(filename)
+        mandelbrot_image(-2.0, 0.5, -1.25, 1.25, cmap='viridis')
 
-def save_image(fig):
-    filename = "mandelbrot.png"
-    fig.savefig(filename)
+class Invader:
+    """
+    """
+    origDimension = 1500
 
-    mandelbrot_image(-2.0, 0.5, -1.25, 1.25, cmap='viridis')
+    r = lambda: random.randint(50,255)
+    rc = lambda: ('#%02X%02X%02X' % (r(),r(),r()))
+    listSym = []
+
+    def create_square(border, draw, randColor, element, size):
+        if (element == int(size/2)):
+            draw.rectangle(border, randColor)
+        elif (len(listSym) == element+1):
+            draw.rectangle(border,listSym.pop())
+        else:
+            listSym.append(randColor)
+            draw.rectangle(border, randColor)
+
+    def create_invader(border, draw, size):
+        x0, y0, x1, y1 = border
+        squareSize = (x1-x0)/size
+        randColors = [rc(), rc(), rc(), (0,0,0), (0,0,0), (0,0,0)]
+        incrementer = 1
+        element = 0
+        #
+        for y in range(0, size):
+            incrementer *= -1
+            element = 0
+            for x in range(0, size):
+                topLeftX = x*squareSize + x0
+                topLeftY = y*squareSize + y0
+                botRightX = topLeftX + squareSize
+                botRightY = topLeftY + squareSize
+                #
+                create_square((topLeftX, topLeftY, botRightX, botRightY), draw, random.choice(randColors), element, size)
+                if (element == int(size/2) or element == 0):
+                    incrementer *= -1;
+                element += incrementer
+
+    def run(size, invaders, imgSize):
+        #
+        origDimension = imgSize
+        origImage = Image.new('RGB', (origDimension, origDimension))
+        draw = ImageDraw.Draw(origImage)
+        #
+        invaderSize = origDimension/invaders
+        print(invaderSize)
+        padding = invaderSize/size
+        print(padding)
+        # Will eventually create many
+        finalBotRightX = 0
+        finalBotRightY = 0
+        for x in range(0, invaders):
+            for y in range(0, invaders):
+                topLeftX = x*invaderSize + padding
+                topLeftY = y*invaderSize + padding
+                botRightX = topLeftX + invaderSize - padding*2
+                botRightY = topLeftY + invaderSize - padding*2
+
+                finalBotRightX = botRightX
+                finalBotRightY = botRightY
+                create_invader((topLeftX, topLeftY, botRightX, botRightY), draw, size)
+        #    origImage.save("Examples/Example-"+str(size)+"x"+str(size)+"-"+str(invaders)+"-"+str(imgSize)+".jpg")
+        origImage.crop((0, 0, botRightX-padding, botRightY-padding)).save("Examples/paddingTest.jpg")
