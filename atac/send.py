942cc579488ea3ca7def8f9da253259384ebea4d
strikles <strikles@gmail.com>
2021-11-26 16:05:48 +0000

    samila

--- atac/send.py
+++ atac/send.py
@@ -1,26 +1,27 @@
 from .compose import AllTimeHigh
 
-import os
-import sys
-import smtplib
-import ssl
 import csv
+from envelope import Envelope
 import frontmatter
+import os
+import smtplib
+import socket
+import ssl
+import sys
 import textwrap
 import time
 from tqdm import tqdm
 
-from envelope import Envelope
-from validate_email import validate_email
-import validators
-
 import phonenumbers
 from phonenumbers import NumberParseException, phonenumberutil
 
-from twilio.rest import Client as TwilioClient
-
 if os.environ.get('DISPLAY'):
     from pywhatkit import *
+
+from twilio.rest import Client as TwilioClient
+
+from validate_email import validate_email
+import validators
 
 
 class FromRuXiaWithLove(AllTimeHigh):
@@ -636,3 +637,59 @@ class FromRuXiaWithLove(AllTimeHigh):
         #
         return status
         """
+
+
+class IRC:
+
+    irc = socket.socket()
+
+    def __init__(self):
+        # Define the socket
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send(self, channel, msg):
+        # Transfer data
+        self.irc.send(bytes("PRIVMSG " + channel + " " + msg + "\n", "UTF-8"))
+
+    def connect(self, server, port, channel, botnick, botpass, botnickpass):
+        # Connect to the server
+        print("Connecting to: " + server)
+        self.irc.connect((server, port))
+
+        # Perform user authentication
+        self.irc.send(bytes("USER " + botnick + " " + botnick +" " + botnick + " :python\n", "UTF-8"))
+        self.irc.send(bytes("NICK " + botnick + "\n", "UTF-8"))
+        self.irc.send(bytes("NICKSERV IDENTIFY " + botnickpass + " " + botpass + "\n", "UTF-8"))
+        time.sleep(5)
+
+        # join the channel
+        self.irc.send(bytes("JOIN " + channel + "\n", "UTF-8"))
+
+    def get_response(self):
+        time.sleep(1)
+        # Get the response
+        resp = self.irc.recv(2040).decode("UTF-8")
+
+        if resp.find('PING') != -1:
+            self.irc.send(bytes('PONG ' + resp.split().decode("UTF-8") [1] + '\r\n', "UTF-8"))
+
+            return resp
+
+    def run(self):
+        ## IRC Config
+        server = "10.x.x.10" # Provide a valid server IP/Hostname
+        port = 6697
+        channel = "#python"
+        botnick = "techbeamers"
+        botnickpass = "guido"
+        botpass = "<%= @guido_password %>"
+        #
+        irc = IRC()
+        irc.connect(server, port, channel, botnick, botpass, botnickpass)
+        #
+        while True:
+            text = irc.get_response()
+            print(text)
+
+            if "PRIVMSG" in text and channel in text and "hello" in text:
+                irc.send(channel, "Hello!")
