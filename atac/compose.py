59474bda185df557fe9e457eaa244732847b0317
strikles <strikles@gmail.com>
2021-12-02 07:24:20 +0000

    send

--- atac/compose.py
+++ atac/compose.py
@@ -224,7 +224,7 @@ class AllTimeHigh(Config):
         msg_image_header.add_header('Content-ID', '<header>')
         message.attach(msg_image_header)
         #
-        sfp = open('branches.gif', 'rb')
+        sfp = open('sudoku.gif', 'rb')
         msg_image_signature = MIMEImage(sfp.read())
         sfp.close()
         # Define the image's ID as referenced above
