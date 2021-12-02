6ec0b704cafe0d3dc689ab3cd81eec32a3b15643
strikles <strikles@gmail.com>
2021-12-02 18:46:09 +0000

    content

--- atac/DFT_epicycle.py
+++ atac/DFT_epicycle.py
@@ -139,7 +139,7 @@ def generate_fourier_epicycles_drawing():
     # original drawing
     orig_drawing, = ax.plot([], [], 'g-', linewidth=0.5)
     # to fix the size of figure so that the figure does not get cropped/trimmed
-    ax.set_xlim(xlim_data[0] - 2, xlim_data[1] + 2)
+    ax.set_xlim(xlim_data[0] - 50, xlim_data[1] + 50)
     ax.set_ylim(ylim_data[0], ylim_data[1])
     # hide axes
     ax.set_axis_off()
