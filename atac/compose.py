53988266b53f3a6c945f3675eaff387427545d93
strikles <strikles@gmail.com>
2021-12-03 10:54:43 +0000

    colete

--- atac/compose.py
+++ atac/compose.py
@@ -228,7 +228,7 @@ class AllTimeHigh(Config):
         #
         # Daniel's Coding Train JavaScript coordinates to be JSON and that will run well.
         z = Epicycles.read_image_as_complex("img/cybertorture.jpg", num_indicies=1700, indices_step_size=5)
-        fourier_data = fourier.fft(z)
+        fourier_data = fft(z)
         # Sort so that largest epicycles are at the center, and the smaller ones are at the location of the drawing points
         fourier_data.sort(key=lambda x: x.amplitude, reverse=True)
         epicycles = Epicycles(fourier_data, plot_size=[427, 427])
