bc9ffd939acaf01f408929c465b1feb9a43ae9d6
strikles <strikles@gmail.com>
2021-12-06 10:19:00 +0000

    colete

--- atac/tests/test_send.py
+++ atac/tests/test_send.py
@@ -66,14 +66,14 @@ def test_send_email():
     message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
     status = katie.send_email(mailing_list, message)
     assert(status == 0) is True
-
+    #
     subject = "RTP2 - Rubrica Aconteçe parte 2 - A árvore dos cagalhões - Cibertortura, Cibersodomia e angariação de ódios entre a população - Exoneração imediata de ex-alunos do colégio militar associados a Pedro Miguel De Brito Esteves Grilo"
     message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/ferronha_colete.md", "message"))
     mailing_list = random.sample(recipients, 1).pop()
     message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
     status = katie.send_email(mailing_list, message)
     assert(status == 0) is True
-    
+    #
     subject = "RTP2 - Rubrica Aconteçe parte 3 - A árvore dos cagalhões - Cibertortura, Cibersodomia e angariação de ódios entre a população - Exoneração imediata de ex-alunos do colégio militar associados a Pedro Miguel De Brito Esteves Grilo"
     message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_duarte.md", "message"))
     mailing_list = random.sample(recipients, 1).pop()
