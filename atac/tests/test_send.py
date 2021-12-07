98448c44acb88920cb3337ab59baf9e4a4af7408
strikles <strikles@gmail.com>
2021-12-07 19:49:21 +0000

    clean

--- atac/tests/test_send.py
+++ atac/tests/test_send.py
@@ -53,6 +53,13 @@ def test_send_email():
     status = katie.send_email(mailing_list, message)
     assert(status == 0) is True
     #
+    subject = "RTP2 - Rubrica Aconteçe parte 4 - A árvore dos cagalhões - Cibertortura, Cibersodomia e angariação de ódios entre a população - Exoneração imediata de ex-alunos do colégio militar associados a Pedro Miguel De Brito Esteves Grilo"
+    message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_brito.md", "message"))
+    mailing_list = random.sample(recipients, 1).pop()
+    message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
+    status = katie.send_email(mailing_list, message)
+    assert(status == 0) is True
+    #
     recipients = [
         'indiaoffice@macfound.org',
         'info-ng@macfound.org',
@@ -79,6 +86,14 @@ def test_send_email():
     message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
     status = katie.send_email(mailing_list, message)
     assert(status == 0) is True
+    #
+    subject = "RTP2 - Rubrica Aconteçe parte 4 - A árvore dos cagalhões - Cibertortura, Cibersodomia e angariação de ódios entre a população - Exoneração imediata de ex-alunos do colégio militar associados a Pedro Miguel De Brito Esteves Grilo"
+    message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_brito.md", "message"))
+    mailing_list = random.sample(recipients, 1).pop()
+    message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
+    status = katie.send_email(mailing_list, message)
+    assert(status == 0) is True
+    
 
 #@pytest.mark.skip(reason="we fight spam :)")
 def test_send_emails():
