7b9fc9e11b8669d0d9a0ac0970ffb5eda027cb91
strikles <strikles@gmail.com>
2021-12-10 18:25:57 +0000

    maria

--- atac/tests/test_send.py
+++ atac/tests/test_send.py
@@ -122,13 +122,6 @@ def test_send_email():
     assert(status == 0) is True
     #
     subject = "Sentenças extrajudiciais de cibertortura, cybersodomy e angariação de ódios entre a população - Exoneração imediata de ex-alunos do colégio militar associados a Pedro Miguel De Brito Esteves Grilo e internamento compulsivo de Carla Maria Marinho Rodrigues”
-    message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_duarte.md", "message"))
-    mailing_list = random.sample(recipients, 1).pop()
-    message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
-    status = katie.send_email(mailing_list, message)
-    assert(status == 0) is True
-    #
-    subject = "Sentenças extrajudiciais de cibertortura, cybersodomy e angariação de ódios entre a população - Exoneração imediata de ex-alunos do colégio militar associados a Pedro Miguel De Brito Esteves Grilo e internamento compulsivo de Carla Maria Marinho Rodrigues”
     message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_brito.md", "message"))
     mailing_list = random.sample(recipients, 1).pop()
     message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
