20dc2222d7d2f570b80554a977d48948950b81a4
strikles <58505677+strikles@users.noreply.github.com>
2022-04-26 19:24:03 +0200

    test_send

--- atac/tests/test_send.py
+++ atac/tests/test_send.py
@@ -33,13 +33,6 @@ def test_send_email():
     auth, _ = katie.get_email_config()
     mailing_list = ";".join(recipients) #random.sample(recipients, 1).pop()
     #
-    subject = "Elect the colégio mitar alumni Diogo Calado the dumbest fighter of all time"
-    message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_calado.md", "message"))
-    mailing_list = random.sample(recipients, 1).pop()
-    message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
-    status = katie.send_email(mailing_list, message)
-    assert(status == 0) is True
-    #
     subject = "Elect José Ricardo Nazareth Carvalho Figueira and Luis Nazareth Carvalho Figueira the dumbest colegio militar alumni of all time"
     message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_figueira.md", "message"))
     mailing_list = random.sample(recipients, 1).pop()
@@ -47,39 +40,12 @@ def test_send_email():
     status = katie.send_email(mailing_list, message)
     assert(status == 0) is True
     #
-    subject = "Elect Carla Maria Marinho Rodrigues the dumbest women in the world for participating in cybertorture and cybersodomy sex parties to force extrajudicial sentences of genital self-harm and compulsory psychiatric care in criminal association with Portuguese law enforcement after turning my father into a cripple"
-    message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_maria.md", "message"))
-    mailing_list = random.sample(recipients, 1).pop()
-    message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
-    status = katie.send_email(mailing_list, message)
-    assert(status == 0) is True
-    #
-    subject = "Elect the former colegio militar student (from largo da Luz, Portugal) Pedro Miguel de Brito Esteves Grilo @PSP Portugal the dumbest policeman in the world"
-    message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_grilo.md", "message"))
-    mailing_list = random.sample(recipients, 1).pop()
-    message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
-    status = katie.send_email(mailing_list, message)
-    assert(status == 0) is True
-    #
-    subject = "Elect the former colegio militar student (from largo da Luz, Portugal) Nuno Poiares @PSP Portugal the dumbest policeman from sci division in the world"
-    message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_poiares.md", "message"))
-    mailing_list = random.sample(recipients, 1).pop()
-    message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
-    status = katie.send_email(mailing_list, message)
-    assert(status == 0) is True
     
     recipients = [
         'indiaoffice@macfound.org',
         'info-ng@macfound.org',
         '4answers@macfound.org'
     ]
-    #
-    subject = "This is a request for emergency humanitarian help to address the threat the hate mongering Portuguese police represents to the families of their victims of cybertorture paired with psychiatric abuse"
-    message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/test_message_humanitarian.md", "message"))
-    mailing_list = random.sample(recipients, 1).pop()
-    message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
-    status = katie.send_email(mailing_list, message)
-    assert(status == 0) is True
     #
     subject = "Madeline McCann, Cybertorture extrajudicial trial and sentences and the hate mongering Portuguese Police"
     message_content = '\n'.join(katie.get_file_content(os.getcwd() + "/atac/tests/ferronha_colete.md", "message"))
@@ -98,15 +64,5 @@ def test_send_emails():
     message_file_path = os.getcwd() + "/atac/tests/test_message.md"
     #
     subject = "Madeline McCann, Cybertorture extrajudicial trial and sentences and the hate mongering Portuguese Police"
-    status = katie.send_emails(email_files_path, message_file_path, subject)
-    assert(status == 0) is True
-    #
-    message_file_path = os.getcwd() + "/atac/tests/test_message_figueira.md"
-    subject = "This is a request for emergency humanitarian help to address the threat the Portuguese police represents to the families of their cybertorture victims"
-    status = katie.send_emails(email_files_path, message_file_path, subject)
-    assert(status == 0) is True
-    #
-    message_file_path = os.getcwd() + "/atac/tests/test_message_calado.md"
-    subject = "Elect the colégio mitar alumni Diogo Calado the dumbest fighter of all time"
     status = katie.send_emails(email_files_path, message_file_path, subject)
     assert(status == 0) is True
