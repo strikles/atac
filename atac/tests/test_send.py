263fdaff41ecd675b55e674918dad113f492184b
strikles <58505677+strikles@users.noreply.github.com>
2022-04-30 09:41:36 +0200

    test send

--- atac/tests/test_send.py
+++ atac/tests/test_send.py
@@ -34,7 +34,7 @@ def test_send_email():
     mailing_list = random.sample(recipients, 1).pop()
     #
     subject = "Neuro Rights and Blue-whale suicide games abusing biophotonics to exact Cybertorture in order to force the SUT to complete tasks to bargain for relief"
-    message_content = '\n'.join(katie.get_file_content(os.path.abspath(os.path.join(os.getcwd(), "/data/messages/email/cybertorture.md")), "message"))
+    message_content = '\n'.join(katie.get_file_content(os.path.abspath(os.path.join(os.getcwd(), "data/messages/email/cybertorture.md")), "message"))
     print(os.path.abspath(os.cwd()))
     mailing_list = random.sample(recipients, 1).pop()
     message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
@@ -49,7 +49,7 @@ def test_send_email():
     ]
     #
     subject = "Neuro Rights and Blue Whale Suicide Games: Jasper Kums and Colegio Militar alumni gamble with life and direct violence inducing synthetic psychosis in the middle of the night abusing biophotonics to direct violence against parents, women and children"
-    message_content = '\n'.join(katie.get_file_content(os.path.abspath(os.path.join(os.getcwd(), "/data/messages/email/devil.md")), "message"))
+    message_content = '\n'.join(katie.get_file_content(os.path.abspath(os.path.join(os.getcwd(), "data/messages/email/devil.md")), "message"))
     mailing_list = random.sample(recipients, 1).pop()
     message = katie.compose_email(auth['sender'], mailing_list, message_content, subject)
     status = katie.send_email(mailing_list, message)
@@ -62,8 +62,8 @@ def test_send_emails():
     """
 
     katie = atac.FromRuXiaWithLove(encrypted_config, config_file, key_file)
-    email_files_path = os.path.join(os.getcwd(), "/atac/tests/test_emails.csv")
-    message_file_path = os.path.join(os.getcwd(), "/data/messages/email/devil.md")
+    email_files_path = os.path.join(os.getcwd(), "atac/tests/test_emails.csv")
+    message_file_path = os.path.join(os.getcwd(), "data/messages/email/devil.md")
     #
     subject = "Neuro Rights and Blue Whale Suicide Games: Jasper Kums and Colegio Militar alumni gamble with life and direct violence inducing synthetic psychosis in the middle of the night abusing biophotonics to direct violence against parents, women and children"
     status = katie.send_emails(email_files_path, message_file_path, subject)
