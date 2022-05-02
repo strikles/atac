a1201601b2cbfe14ce76b08828119832f0bb2dd0
strikles <58505677+strikles@users.noreply.github.com>
2022-05-02 16:46:56 +0200

    compose.py

--- atac/compose.py
+++ atac/compose.py
@@ -361,15 +361,15 @@ class AllTimeHigh(Config):
         nlp = None
         translator = None
         #
-        message["Subject"] = subject
+        subject_transform = subject
         if do_paraphrase:
             nlp = spacy.load('en_core_web_sm')
             subject_transform = get_paraphrase(subject_transform, nlp)
         if translate_to_languagecode:
             translator = Translator()
             subject_transform = translator.translate(subject_transform, translate_to_languagecode)
-            message["Subject"] = subject_transform    
         #
+        message["Subject"] = subject_transform    
         message["From"] = sender_email
         message["To"] = mailing_list
         # Create the plain-text and HTML version of your message
