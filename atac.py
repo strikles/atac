737e8efdffbc93f2f2576418589271d86cbdec28
strikles <58505677+strikles@users.noreply.github.com>
2022-05-05 16:07:49 +0200

    util.py + atac.py

--- atac.py
+++ atac.py
@@ -5,17 +5,6 @@ from threading import Thread
 import atac
 
 
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-
 def get_config_arguments(arguments):
     """
     Generate New Config
--- atac/util.py
+++ atac/util.py
@@ -5,3 +5,16 @@ def trace(f):
         return f(*args, **kwargs)
 
     return wrap
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    try:
+        raise TypeError()
+    except (ValueError, TypeError) as exception:
+        print("Catching all exceptions")
