eb3dc03175edcb9bce5d9b6ec6be4b04c31b4495
strikles <58505677+strikles@users.noreply.github.com>
2022-05-06 11:37:59 +0200

    data + compose.py

--- atac/compose.py
+++ atac/compose.py
@@ -182,9 +182,14 @@ class AllTimeHigh(Config):
             <meta charset='utf-8'>
             <meta name='viewport' content='width=device-width'>
             <title>MathJax example</title>
+            <style type="text/css">
+                p {margin:25px 77px;}
+                ol {margin:25px 77px;}
+                ul {margin:25px 77px;}
+                li p {margin:5px;}
+            </style>
             <script src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script>
-            <script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'>
-            </script>
+            <script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>
         </head>
         </body>
         """
